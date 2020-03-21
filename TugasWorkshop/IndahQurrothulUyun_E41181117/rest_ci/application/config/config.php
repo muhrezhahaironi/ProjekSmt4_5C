01beb26a8d4aba8e8ca526ab39b6ceb16d621800
indahqurrothul <56025305+indahqurrothul@users.noreply.github.com>
2020-03-21 20:23:18 +0700

    RestApi_Indah

--- TugasWorkshop/IndahQurrothulUyun_E41181117/rest_ci/application/config/config.php
+++ TugasWorkshop/IndahQurrothulUyun_E41181117/rest_ci/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rest_ci/';
+$config['base_url'] = 'http://localhost/ProjekSmt4_5C/TugasWorkshop/IndahQurrothulUyun_E41181117rest_ci/';
 
 /*
 |--------------------------------------------------------------------------
